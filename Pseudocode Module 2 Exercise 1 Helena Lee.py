def379c9bf5c651c0f94c1ec299e79128c6617f7
mlee280 <mlee280@ivytech.edu>
2024-09-01 09:31:38 +0100

    Add files via upload

---
+++ Pseudocode Module 2 Exercise 1 Helena Lee.py
@@ -0,0 +1,31 @@
+Exercise 1
+
+
+    
+// Design a program that prompts the user for a refrigerator model name and the
+// interior height, width, and depth in inches.
+    
+
+START
+
+        input RefrigeratorModelName
+        input RefrigeratorInteriorHeightInInches
+        input RefrigeratorInteriorWidthInInches
+        input RefrigeratorInteriorDepthInInches
+        
+
+        CubicFeet = (Height * Width * Depth) /1728
+
+        outputRefrigeratorModel
+        outputCubicFeet
+
+STOP
+
+
+
+
+
+
+
+
+
---
+++ Pseudocode Module 2 Exercise 2 Helena Lee.py
@@ -0,0 +1,56 @@
+Exeercise 2
+
+// (Plan)The program determines a monthly checking account fee.
+
+START
+
+    accountBalance = 0
+    overDrawnFee = which is 1 percent of the balance minus 5 dollars for each time the account was overdrawn
+    
+    input AccountBalance
+    input NumberOfTimesAccountWasOverdrawn
+
+    ComputeTheFee
+    ComputeTheAccountBalance - Fee
+    ComputeTheNewAccountBalance
+
+    output Fee
+    output NewAccountBalance
+    output ThanksForUsingThisProgram
+
+STOP
+
+
+Exercise 3
+// This program accepts a user's monthly pay
+// and rent, utilities, and grocery bills
+// and displays the amount available for discretionary spending
+// (which might be negative)
+// Your program should output the pay and the total bills
+// as well as the remaining discretionary amount
+
+
+START
+
+    input monthlyPay
+    input monthlyRent
+    input monthlyCostOfUtilities
+    input monthlyCostofGroceries
+
+    totalBills = monthlyRent + monthlyCostOfUtilities + monthlyCostofGroceries
+
+    output monthlyPay
+    output totalBills
+    output monthlyPay - totalBills
+
+STOP
+
+
+
+
+
+
+
+
+
+
